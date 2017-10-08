3d64220a7b25e363b7d122e7ff4281973d873bc0
박한별 <qkrgksquf00@naver.com>
2017-10-08 18:20:42 +0900

    Bluetooth

--- MainActivity.java
+++ MainActivity.java
@@ -1,12 +1,34 @@
 package com.example.collisionbike;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.example.collisionbike.BluetoothSerialClient.BluetoothStreamingHandler;
+import com.example.collisionbike.BluetoothSerialClient.OnScanListener;
+
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayAdapter<String> mDeviceArrayAdapter;
+    private LinkedList<BluetoothDevice> mBluetoothDevices = new LinkedList<BluetoothDevice>();
+    private ProgressDialog mLoadingDialog;
+    private BluetoothSerialClient mClient;
+
+    private AlertDialog mDeviceListDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +56,160 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ImageView iv3 = (ImageView)findViewById(R.id.BluetoothImage);
+
+        iv3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                mClient = BluetoothSerialClient.getInstance();
+
+                initProgressDialog();
+                initDeviceListDialog();
+                mDeviceListDialog.show();
+            }
+        });
+    }
+    private void initDeviceListDialog() {
+        mDeviceArrayAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.item_device);
+        ListView listView = new ListView(getApplicationContext());
+        listView.setAdapter(mDeviceArrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item =  (String) parent.getItemAtPosition(position);
+                for(BluetoothDevice device : mBluetoothDevices) {
+                    if(item.contains(device.getAddress())) {
+                        connect(device);
+                        mDeviceListDialog.cancel();
+                    }
+                }
+            }
+        });
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select bluetooth device");
+        builder.setView(listView);
+        builder.setPositiveButton("Scan",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        scanDevices();
+                    }
+                });
+        mDeviceListDialog = builder.create();
+        mDeviceListDialog.setCanceledOnTouchOutside(false);
+    }
+
+    private void connect(BluetoothDevice device) {
+        mLoadingDialog.setMessage("Connecting....");
+        mLoadingDialog.setCancelable(false);
+        mLoadingDialog.show();
+        BluetoothSerialClient btSet =  mClient;
+        boolean connect = btSet.connect(getApplicationContext(), device, mBTHandler);
+    }
+
+    private void scanDevices() {
+        com.example.collisionbike.BluetoothSerialClient btSet = mClient;
+        btSet.scanDevices(getApplicationContext(), new OnScanListener() {
+            String message ="";
+            @Override
+            public void onStart() {
+                Log.d("Test", "Scan Start.");
+                mLoadingDialog.show();
+                message = "Scanning....";
+                mLoadingDialog.setMessage("Scanning....");
+                mLoadingDialog.setCancelable(true);
+                mLoadingDialog.setCanceledOnTouchOutside(false);
+                mLoadingDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        com.example.collisionbike.BluetoothSerialClient btSet = mClient;
+                        btSet.cancelScan(getApplicationContext());
+                    }
+                });
+            }
+
+            @Override
+            public void onFoundDevice(BluetoothDevice bluetoothDevice) {
+                addDeviceToArrayAdapter(bluetoothDevice);
+                message += "\n" + bluetoothDevice.getName() + "\n" + bluetoothDevice.getAddress();
+                mLoadingDialog.setMessage(message);
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d("Test", "Scan finish.");
+                message = "";
+                mLoadingDialog.cancel();
+                mLoadingDialog.setCancelable(false);
+                mLoadingDialog.setOnCancelListener(null);
+                mDeviceListDialog.show();
+            }
+        });
+    }
+
+
+    private void addDeviceToArrayAdapter(BluetoothDevice device) {
+        if(mBluetoothDevices.contains(device)) {
+            mBluetoothDevices.remove(device);
+            mDeviceArrayAdapter.remove(device.getName() + "\n" + device.getAddress());
+        }
+        mBluetoothDevices.add(device);
+        mDeviceArrayAdapter.add(device.getName() + "\n" + device.getAddress() );
+        mDeviceArrayAdapter.notifyDataSetChanged();
+
+    }
+
+    private BluetoothStreamingHandler mBTHandler = new BluetoothStreamingHandler() {
+        ByteBuffer mmByteBuffer = ByteBuffer.allocate(1024);
+
+        @Override
+        public void onError(Exception e) {
+            mLoadingDialog.cancel();
+        }
+
+        @Override
+        public void onConnected() {
+
+        }
+
+        @Override
+        public void onDisconnected() {
+            mLoadingDialog.cancel();
+        }
+        @Override
+        public void onData(byte[] buffer, int length) {
+            if(length == 0) return;
+            if(mmByteBuffer.position() + length >= mmByteBuffer.capacity()) {
+                ByteBuffer newBuffer = ByteBuffer.allocate(mmByteBuffer.capacity() * 2);
+                newBuffer.put(mmByteBuffer.array(), 0,  mmByteBuffer.position());
+                mmByteBuffer = newBuffer;
+            }
+            mmByteBuffer.put(buffer, 0, length);
+            if(buffer[length - 1] == '\0') {
+                //addText(mClient.getConnectedDevice().getName() + " : " +
+                        //new String(mmByteBuffer.array(), 0, mmByteBuffer.position()) + '\n');
+                mmByteBuffer.clear();
+            }
+        }
+    };
+
+    private void initProgressDialog() {
+        mLoadingDialog = new ProgressDialog(this);
+        mLoadingDialog.setCancelable(false);
+    }
+
+    @Override
+    protected void onPause() {
+        mClient.cancelScan(getApplicationContext());
+        super.onPause();
+    }
+
+    private void getPairedDevices() {
+        Set<BluetoothDevice> devices =  mClient.getPairedDevices();
+        for(BluetoothDevice device: devices) {
+            addDeviceToArrayAdapter(device);
+        }
     }
 }
+
